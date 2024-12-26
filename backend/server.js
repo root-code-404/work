af77d93279c1366e8c28dae6d5a33a44da311e5e
Muhsin I <muhsinishac@gmail.com>
2024-12-26 18:38:43 +0530

    fourth commit

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,12 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(
+    cors({
+        origin: ['https://mo4work.netlify.app/'],
+        credentials: true,
+    })
+);
 app.use(express.json());
 app.use('/uploads', express.static('uploads')); // Serve uploaded images as static files
 
