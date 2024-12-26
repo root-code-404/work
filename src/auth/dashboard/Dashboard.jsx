0a901984ead6a649088c9190cb86204be188729d
Muhsin I <muhsinishac@gmail.com>
2024-12-26 16:47:38 +0530

    second commit

--- src/auth/dashboard/Dashboard.jsx
+++ src/auth/dashboard/Dashboard.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate, useParams } from "react-router-dom";
-import Sidebar from "../../components/Sidebar/sidebar";
+
 
 // Dashboard Component
 const Dashboard = () => {
